46d2df0da3986c86d0d863338352b5eec0d083d6
amosrodriguesbyui <rod23045@byui.edu>
2024-06-15 11:47:18 -0100

    C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+
+        Random magicNumber = new Random();
+        int magic = magicNumber.Next(1,11);
+
+        int guess = 0;
+
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            string guessNumber = Console.ReadLine();
+            guess = int.Parse(guessNumber);
+        
+            if (magic > guess)
+            {
+                Console.WriteLine("Highier");
+            }
+            else if (magic < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guess it!");
+            }
+        } while (guess != magic);
     }
 }
